32ce993bf70c779581117e9fb6b2d4de4569cadb
Robin Glattauer <robin.glattauer@assoc.oeaw.ac.at>
2012-01-27 16:04:14 +0000

    added coordinate systems

--- ild/util/MeasurementSurface.cc
+++ ild/util/MeasurementSurface.cc
@@ -1,1 +1,3 @@
 #include "MeasurementSurface.h"
+
+
--- ild/util/MeasurementSurface.h
+++ ild/util/MeasurementSurface.h
@@ -1,6 +1,36 @@
-#ifndef MEASUREMENTSURFACE_HH
-#define MEASUREMENTSURFACE_HH 1
+#ifndef MEASUREMENTSURFACE_H
+#define MEASUREMENTSURFACE_H
 
+#include "ICoordinateSystem.h"
 
+/** Measurement Surface Class */
+namespace GearExtensions{
+   
+   
+class MeasurementSurface{
+   
+public:
+   
+   MeasurementSurface( int ID, ICoordinateSystem* csys ): _ID( ID ), _coordinateSystem( csys ){}
+   
+   ~MeasurementSurface(){ delete _coordinateSystem; }
+   
+   int getID(){ return _ID; }
+   ICoordinateSystem* getCoordinateSystem(){ return _coordinateSystem; }
+   
+private:
+   
+   int _ID;
+   ICoordinateSystem* _coordinateSystem;
+   
+   
+   MeasurementSurface(const MeasurementSurface& m){};   // copy constructor is private --> no cpoying allowed
+   MeasurementSurface& operator= (MeasurementSurface const& m); // assignment not allowed either
+   
+   
+};
+   
+   
+} //end of namespace
 
 #endif
